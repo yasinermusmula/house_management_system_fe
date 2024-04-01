8c47ad19b2dffa558b0cfd26243ef7addf4b79a1
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-01 18:10:30 +0300

    Homepage Created

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import Navbar from "../layout/Navbar";
+import HousesIcon from "../layout/HousesComponent";
+import Properties from "../layout/Properties";
+
+
+export default function HomePage(){
+    return(
+        <>
+            <Navbar/>
+            <HousesIcon/>
+            <Properties/>
+        </>
+    )
+}
---
+++ src/data/HomePageData.js
@@ -0,0 +1,39 @@
+import img from "../assets/airbnbprop.jpg"
+
+export const HomePageData = [
+    {
+        propertyImg:img,
+        firstLocation:"Sapanca,Turkey",
+        distance:"120 km uzakta",
+        available:"28Nis-3May",
+        price:"5850"
+    },
+    {
+        propertyImg:img,
+        firstLocation:"Sapanca,Turkey",
+        distance:"120 km uzakta",
+        available:"28Nis-3May",
+        price:"5850"
+    },
+    {
+        propertyImg:img,
+        firstLocation:"Sapanca,Turkey",
+        distance:"120 km uzakta",
+        available:"28 Nis-3May",
+        price:"5850"
+    },
+    {
+        propertyImg:img,
+        firstLocation:"Sapanca,Turkey",
+        distance:"120 km uzakta",
+        available:"28Nis-3May",
+        price:"5850"
+    },
+    {
+        propertyImg:img,
+        firstLocation:"Sapanca,Turkey",
+        distance:"120 km uzakta",
+        available:"28Nis-3May",
+        price:"5850"
+    }
+]
