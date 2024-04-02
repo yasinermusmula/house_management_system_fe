fbaeb9789f763a4500ee1cfdf959e3b85df1daa5
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-02 14:20:10 +0300

    html tag change from div to button

--- src/layout/Properties.js
+++ src/layout/Properties.js
@@ -2,10 +2,10 @@ import {HomePageData} from "../data/HomePageData";
 import PropertyCard from "./PropertyCard";
 export default function Properties(){
     return(
-        <div className="flex flex-wrap gap-12 mx-16 px-16">
+        <button className="flex flex-wrap gap-12 mx-16 px-16">
             {HomePageData.map((property) => (
                 <PropertyCard property = {property}/>
             ))}
-        </div>
+        </button>
     )
 }
