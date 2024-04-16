58fd438b1885b446c6a5140bba595eb56f9003ee
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-16 12:11:16 +0300

    tailwind edited

--- src/layout/LoginPage.js
+++ src/layout/LoginPage.js
@@ -52,7 +52,7 @@ export default function LoginPage(){
                     />
                     <button
                         type="submit"
-                        className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        className="w-full mt-5 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                         disabled={!isValid}
                     >
                         Log in
