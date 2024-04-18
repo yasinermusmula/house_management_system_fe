6bc77cc3d2608d05045a82d2fe1dc98ee2545cca
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-18 21:16:55 +0300

    logout action created. userVerify function added but it isn't work right now

--- src/store/actions/UserActions.js
+++ src/store/actions/UserActions.js
@@ -1,8 +1,11 @@
-import axios from "axios";
+import axios, {Axios} from "axios";
 import {FETCH_STATE} from "../reducers/UserActionReducer";
+import {API} from "../../api/api";
+import {useSelector} from "react-redux";
 
 export const ADD_USER_DATA = "ADD_USER_DATA";
 export const SET_FETCH_STATE= "FETCH_STATE";
+export const SET_LOG_OUT = "SET_LOG_OUT";
 
 export function addUserData(user){
     return {type:ADD_USER_DATA,payload:user}
@@ -12,15 +15,39 @@ export function setFetchState(fetchState){
     return {type:SET_FETCH_STATE,payload:fetchState}
 }
 
+export function setLogOut(){
+    return {type: SET_LOG_OUT}
+}
 export const userName = (data,history) => (dispatch) => {
-    axios.post("http://localhost:8082/api/auth/login",data)
+    API.post("/auth/login",data)
         .then((res)=> {
             dispatch(addUserData(res.data))
             dispatch(setFetchState(FETCH_STATE.FETCHED))
             console.log(res.data.name)
+            localStorage.setItem("token",res.data.password)
             history.push("/")
         })
         .catch((err) => {
             console.log(err)
         })
+}
+
+export const userVerify = (email) =>(dispatch)=> {
+    API.get(`/auth/verify?email=${email}`)
+        .then((res)=> {
+        localStorage.setItem("token",res.data.password);
+        dispatch(addUserData(res.data))
+        dispatch(setFetchState(FETCH_STATE.FETCHED))
+    })
+        .catch((err) =>{
+            console.log(err);
+            localStorage.removeItem("token")
+        })
+}
+
+export const logOutAction = () => (dispatch) => {
+    dispatch(setLogOut())
+    dispatch(setFetchState(FETCH_STATE.NOT_FETCHED))
+    localStorage.removeItem("token");
+    console.log("logout oldu");
 }
